4be9905dfa92431a2d63faa83a4874ea7402950e
adib649 <adishreebatchu@gmail.com>
2025-08-04 12:50:32 -0400

    Update streamlit_app.py
    fixing tabs vs spaces issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,19 +24,18 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    ingredients_string = ''
+  ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen + ' '
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
       
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+  my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
     
-    time_to_insert = st.button('Submit Order')
+  time_to_insert = st.button('Submit Order')
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        
-        st.success('Your Smoothie is ordered,' + ' ' + name_on_order + "!", icon="✅")
+  if time_to_insert:
+    session.sql(my_insert_stmt).collect()
+    st.success('Your Smoothie is ordered,' + ' ' + name_on_order + "!", icon="✅")
